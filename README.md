64caf886b24c632d8a849b9d3f5b6d3b8a140555
R3ZV <razvan.maracine@protonmail.com>
2026-01-28 23:03:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# References
+- https://en.wikipedia.org/wiki/Infinite_impulse_response
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <vector>
+
+int
+main() {
+    std::vector<int16_t> sample;
+
+    return 0;
+}
